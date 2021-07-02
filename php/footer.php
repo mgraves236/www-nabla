e7ffb85284b3f737a4720915b89ec3f4b6ee079e
laitacende <aleksandra.golebiowska600@gmail.com>
2021-07-02 22:53:47 +0200

    .

--- php/footer.php
+++ php/footer.php
@@ -5,7 +5,7 @@
         <i class="fab fa-youtube"></i>
         <i class="far fa-envelope"></i>
     </address>
-    &copy Copyright PSS Nabla | <a href="https://github.com/laitacende">Aleksandra Gołębiowska</a> &
-    <a href="https://github.com/mgraves236">Magdalena Gołębiowska </a>
+    &copy Copyright PSS Nabla | <a href="https://github.com/laitacende" target="_blank">Aleksandra Gołębiowska</a> &
+    <a href="https://github.com/mgraves236" target="_blank">Magdalena Gołębiowska </a>
 </footer>
 <script src ="js/menu.js"></script>
--- php/main_nav.php
+++ php/main_nav.php
@@ -6,7 +6,7 @@
                 <li><a href="#" class="active">Home</a></li>
                 <li><a href="#">News</a></li>
                 <li><a href="#">Members</a></li>
-                <li><a href="#">Projects</a></li>
+                <li><a href="#" class="active">Projects</a></li>
             </ul>
         </nav>
         <span id="hamburger">&#8801;</span>
