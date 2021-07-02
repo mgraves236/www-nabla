2a0e6047395bf62973fab40a61b6e8e612fbf385
laitacende <aleksandra.golebiowska600@gmail.com>
2021-07-02 22:25:15 +0200

    .

--- php/footer.php
+++ php/footer.php
@@ -5,6 +5,7 @@
         <i class="fab fa-youtube"></i>
         <i class="far fa-envelope"></i>
     </address>
-    &copy Copyright PSS Nabla | Aleksandra Gołębiowska & Magdalena Gołębiowska
+    &copy Copyright PSS Nabla | <a href="https://github.com/laitacende">Aleksandra Gołębiowska</a> &
+    <a href="https://github.com/mgraves236">Magdalena Gołębiowska </a>
 </footer>
 <script src ="js/menu.js"></script>
