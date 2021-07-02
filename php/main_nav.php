f2fd12ee817a634511512a9988567b3cc7580fb0
laitacende <aleksandra.golebiowska600@gmail.com>
2021-07-02 15:15:10 +0200

    .

---
+++ php/main_nav.php
@@ -0,0 +1,8 @@
+<nav class="main-nav">
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">News</a></li>
+        <li><a href="#">Members</a></li>
+        <li><a href="#">Projects</a></li>
+    </ul>
+</nav>
