c19a689ffe00dbf7b2e875733c996870f774eee1
laitacende <aleksandra.golebiowska600@gmail.com>
2021-07-02 22:21:50 +0200

    .

--- php/main_nav.php
+++ php/main_nav.php
@@ -2,7 +2,7 @@
     <div class="header-main">
         <img alt="nabla-logo" src="img/nvbla.svg">
         <nav class="main-nav">
-            <ul class="nav-list blur hide">
+            <ul class="nav-list hide">
                 <li><a href="#" class="active">Home</a></li>
                 <li><a href="#">News</a></li>
                 <li><a href="#">Members</a></li>
