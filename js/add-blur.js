d515db9f844c44a4e9ace07c7d84dc027e35fad3
mgraves236 <m.graves236@gmail.com>
2021-07-02 18:07:27 +0200

    .

--- js/add-blur.js
+++ js/add-blur.js
@@ -1,5 +1,7 @@
-if ($(window).width() < 1500) {
-    $('#space').addClass('blur');
-} else {
-    $('#space').removeClass('blur');
-}
+$(window).resize(function() {
+    if ($(window).width() < 1500) {
+        $('#space').addClass('blur');
+    } else {
+        $('#space').removeClass('blur');
+    }
+}).resize(); // trigger resize event initially
