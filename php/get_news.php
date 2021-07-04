2663ba46b45695a8244ddcd8c18371aba2fc7956
laitacende <aleksandra.golebiowska600@gmail.com>
2021-07-04 12:25:01 +0200

    .

--- php/get_news.php
+++ php/get_news.php
@@ -3,14 +3,17 @@
     $databaseDirectory = "db/nabla";
     $newsStore = new \SleekDB\Store("news", $databaseDirectory);
 
+
     $news = $newsStore
     ->createQueryBuilder()
     ->where( [ "date", "=", "zimowy_2020-2021" ] )
+        ->orderBy(["_id"=>"desc"])
     ->getQuery()
     ->fetch();
     foreach ($news as &$value) {
         echo '<article class="news blur">'. '<img src="' .
-                $value['img'] . '">'
+                $value['img'] . '">' .
+            '<p>'. $value['des'] . '</p>'
          .'</article>';
     }
 
